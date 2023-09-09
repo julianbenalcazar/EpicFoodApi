2fec6b7d815e918e72b166e6a697d2c752df3e2f
Julian Benalcazar <oswaldobenal12@gmail.com>
2023-09-09 18:04:46 -0500

    Cloudinary secret

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -6,7 +6,7 @@ export const CloudinaryProvider = {
     cloudinary.config({
       cloud_name: 'dtnqkbuxn',
       api_key: '167456968231542',
-      api_secret: 'c_-O0na1Ba0cShw6V3aBdkq1x5o',
+      api_secret: `${process.env.CLOUDINARY_API_SECRET}`,
     });
   },
 };
